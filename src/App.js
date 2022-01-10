1f0c00116d095691e6aea2d5972fc5334f082128
Jun <bearwcw@naver.com>
2022-01-10 21:25:16 +0900

    Update toDos part.2

--- src/App.js
+++ src/App.js
@@ -10,11 +10,12 @@ function App() {
     event.preventDefault();    
     if(toDo === "")
       return;
-      setToDos(currentArray => [toDo, ...currentArray]);
-      setToDo("");          
-  };  
-  console.log(toDos)
 
+    //setToDos는 현재 state 값을 받아온다.
+    setToDos(currentArray => [toDo, ...currentArray]);
+    setToDo("");          
+  };  
+  
   return (
     <div>
       <h1>My To Dos({toDos.length})</h1>
@@ -22,6 +23,11 @@ function App() {
         <input onChange={onChange} value={toDo} type="text" placeholder="Write yout to do.." />
         <button>Add To Do</button>
       </form>
+      <hr />
+      <ul>
+        {toDos.map( (item, index) => 
+          <li key={index}>{item}</li> )}
+      </ul>
     </div>  
   );
 }
